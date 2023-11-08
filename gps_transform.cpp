03c2cf4f9c76fdd172e229653ea9a2d1729abb71
Krishna Dihora <24kjdih2@gmail.com>
2023-11-07 21:00:53 -0500

    Added starter code

---
+++ gps_transform.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+#include <filesystem>
+#include <vector>
+#include <fstream>
+#include <cmath>
+#include <utility>
+#include <deque>
+#include <iomanip>
+
+
+
+using std::string;
+using std::pair;
+
+class GPSdata {
+    
+    public:
+
+    private:
+
+
+
+}
