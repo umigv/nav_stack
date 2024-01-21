1c6dc678978785f34b39ea6155b5ab4f57d89f2a
firestrike007 <cshankhin@gmail.com>
2024-01-21 22:41:20 +0000

    Added: CV, Snsrs occupancy grid subscribers; started on tranformations

--- merge_costmap.cpp
+++ merge_costmap.cpp
@@ -1,22 +1,56 @@
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/OccupancyGrid.msg"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 
 using std::placeholders::_1;
 
 class OccupancyGridSubscriber : public rclcpp::Node {
     public:
         OccupancyGridSubscriber() : Node("occupancy_grid_subscriber") {
-        subscription_ = this->create_subscription<nav_msgs::OccupancyGrid>(
-            "/occupancy_grid", 10, std::bind(&OccupancyGridSubscriber::topic_callback, this, _1)
+        cv_og_subsriber_ = this->create_subscription<nav_msgs::OccupancyGrid>(
+            "/occupancy_grid", 10, std::bind(&OccupancyGridSubscriber::populate_cv_occupancy_grid, this, _1)
+        )
+
+        sensors_occupancy_grid_subsriber = this->create_subscription<nav_msgs::OccupancyGrid>(
+            "/occupancy_grid", 10, std::bind(&OccupancyGridSubscriber::populate_sensors_occupancy_grid, this, _1)
         )
     }
 
-    private:
-        // occupancy grid variable
-        int8_t occupancy_grid[][];
 
-        void topic_callback(const nav_msgs::OccupancyGrid &msg) const {
-            occupancy_grid = msg.data;
+    private:
+        // ------------- OCCUPANCY GRID VARIABLES -----------------
+
+        // sensors occupancy grid variable
+        nav_msgs::msg::OccupancyGrid sensors_occupancy_grid;
+
+        // CV occupancy grid variable
+        nav_msgs::msg::OccupancyGrid cv_occupancy_grid;
+
+        // Combined Occupancy Grid variable
+        nav_msgs::msg::OccupancyGrid combined_occupancy_grid;
+        
+
+        // ------------------------CALLBACKS -------------------------
+        // populate cv occupancy grid
+        void populate_cv_occupancy_grid(const nav_msgs::msg::OccupancyGrid::SharedPtr &cv_cm)
+        {
+            //Myabe save snsr metadata once to increase efficency - resolution, width, height, origin
+            cv_occupancy_grid.data = cv_cm->data;
+            cv_occupancy_grid.info = cv_cm->info;
+            cv_occupancy_grid.header = cv_cm->header; //cv_cm??
+        }
+
+        // populate global occupancy grid
+        void populate_sensors_occupancy_grid(const nav_msgs::msg::OccupancyGrid::SharedPtr &snsr_cm)
+        {
+            //Myabe save snsr metadata once to increase efficency - resolution, width, height, origin
+            sensors_occupancy_grid.data = snsr_cm->data;
+            sensors_occupancy_grid.info = snsr_cm->info;
+            sensors_occupancy_grid.header = snsr_cm->header;
         }
 }
+
+
+// subscr to global OG, make a copy of it use that to init our OG
+// writing the transform to get robot curr position ie. tf2
+// overlay part - taking in curr position adn begin overlay
---
+++ tf_listener.cpp
@@ -0,0 +1,57 @@
+#include <rclcpp/rclcpp.hpp>
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2/exceptions.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/buffer.h"
+
+class TfListener : public rclcpp::Node{
+    private:
+    // Subscription to pose published by sensor node
+    rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr pose_sub_;
+    // Listener for the broadcast transform message
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+    // Buffer that stores several seconds of transforms for easy lookup by the listener
+    std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+    // Pose in source frame (`sensor_link`)
+    geometry_msgs::msg::PoseStamped pose_in_;
+    // Pose in target frame (`arm_end_link`)
+    geometry_msgs::msg::PoseStamped pose_out_;
+
+    void poseCallback(const geometry_msgs::msg::PoseStamped::SharedPtr msg){
+        try {
+            pose_in_ = *msg;
+            // Transforms the pose between the source frame and target frame
+            tf_buffer_->transform<geometry_msgs::msg::PoseStamped>(pose_in_, pose_out_, "arm_end_link",
+                tf2::Duration(std::chrono::seconds(1)));
+            // Log coordinates of pose in target frame
+            RCLCPP_INFO(get_logger(),"Object pose in 'arm_end_link' is:\n x,y,z = %.1f,%.1f,%.1f",
+                pose_out_.pose.position.x,
+                pose_out_.pose.position.y,
+                pose_out_.pose.position.z);
+        } catch (const tf2::TransformException & ex){
+            RCLCPP_WARN(get_logger(),"Could not find object position in 'arm_end_link' frame.");
+            return;
+        }
+    }
+    public:
+    TfListener(const std::string & name):
+    Node(name){
+        // Load a buffer of transforms
+        tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+        // Listen to the buffer of transforms
+        tf_listener_ = std::make_unique<tf2_ros::TransformListener>(*tf_buffer_);
+        // Subscribe to pose published by sensor node (check every second)
+        pose_sub_ = create_subscription<geometry_msgs::msg::PoseStamped>
+            ("/detected_object", 10,
+            std::bind(&TfListener::poseCallback, this, std::placeholders::_1));
+    }
+    ~TfListener(){}
+};
+
+int main(int argc, char const *argv[]){
+   rclcpp::init(argc, argv);
+   auto node = std::make_shared<TfListener>("tf_listener");
+   rclcpp::spin(node);
+   return 0;
+}
