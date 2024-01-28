f82ffe35addeb653f7d40b95a9b94ea9ab3e2d3f
Matt Prince <115430383+mdp0225@users.noreply.github.com>
2024-01-28 17:31:25 -0500

    Update merge_costmap.cpp

--- merge_costmap.cpp
+++ merge_costmap.cpp
@@ -4,21 +4,14 @@
 
 using std::placeholders::_1;
 using OccupancyGrid = nav_msgs::msg::OccupancyGrid;
+using Point = geometry_msgs::msg::Point;
 
 // Subsribes to the global occupancy grid and the cv occupancy grid 
 // and merges them together to create a new global occupancy grid
 // that is then published to the global costmap
 class MergeService : public rclcpp::Node {
     public:
-        MergeService() : Node("merge_service") {
-        cv_og_subsriber_ = this->create_subscription<OccupancyGrid>(
-            "/occupancy_grid", 10, std::bind(&MergeService::populate_cv_occupancy_grid, this, _1)
-        )
-
-        sensors_occupancy_grid_subsriber = this->create_subscription<nav_msgs::msg::OccupancyGrid>(
-            "/occupancy_grid", 10, std::bind(&MergeService::populate_sensors_occupancy_grid, this, _1)
-        )
-    }
+        MergeService() : Node("merge_service") {}
 
 
     private:
@@ -41,7 +34,8 @@ class MergeService : public rclcpp::Node {
             //Maybe save snsr metadata once to increase efficency - resolution, width, height, origin
             cv_occupancy_grid.data = cv_cm->data;
             cv_occupancy_grid.info = cv_cm->info;
-            cv_occupancy_grid.header = cv_cm->header; //cv_cm??
+            cv_occupancy_grid.header = cv_cm->header;
+             //cv_cm??
         }
 
         // populate global occupancy grid
@@ -54,21 +48,30 @@ class MergeService : public rclcpp::Node {
         }
 
         //populate combined occupancy grid
-        void populate_combined_occupancy_grid(const OccupancyGrid &cv_cm, const OccupancyGrid &snsr_cm)
+        void populate_combined_occupancy_grid(const OccupancyGrid &merge_cm)
         {
-            //Tf2 local data into global with gps
-            combined_occupancy_grid.info = snsr_cm->info;
-            combined_occupancy_grid.header = snsr_cm->header;
+            combined_occupancy_grid.data = merge_cm->data;
+            combined_occupancy_grid.info = merge_cm->info;
+            combined_occupancy_grid.header = merge_cm->header;
         }
 
-        void tf2_local_to_global()
+        Point tf2_local_to_global(OccupancyGrid &cv, OccupancyGrid &global)
         {
+            Point curr;
+
             
         }
 
-        OccupancyGrid &merge_occupancy_grids()
+        OccupancyGrid &merge_occupancy_grids(OccupancyGrid &cv, OccupancyGrid &global)
         {
-
+            // Merges the cv and global occupancy grids together to create a new global occupancy grid
+            // that is then published to the global costmap
+            OccupancyGrid merged_occupancy_grid;
+            merged_occupancy_grid.data = cv.data;
+            merged_occupancy_grid.info = cv.info;
+            merged_occupancy_grid.header = cv.header;
+            // TODO: merge the two grids together            
+            return merged_occupancy_grid;
         }
 
 }
@@ -77,3 +80,6 @@ class MergeService : public rclcpp::Node {
 // subscr to global OG, make a copy of it use that to init our OG
 // writing the transform to get robot curr position ie. tf2
 // overlay part - taking in curr position adn begin overlay
+
+// You need to use tf2 to get the subscriber to get the robot to transform in the world frame
+// 
