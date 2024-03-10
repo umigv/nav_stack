3d2491802326b787f9447f1000a78a99d6dedf3f
Ben <brossano@umich.edu>
2024-03-10 16:21:59 -0400

    started cv grid

--- cv_grid/src/cv_grid.cpp
+++ cv_grid/src/cv_grid.cpp
@@ -25,36 +25,20 @@ public:
         tf_buffer_ = std::make_unique<tf2_ros::Buffer>(this->get_clock());
         tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 
-        // Origin of static lane lines grid in the global frame 
-        origin_x_ = 0;
-        origin_y_ = 0;
-
-        // Initialize static lane lines grid
-        static_grid_ = std::vector<vector<int>>(1200, std::vector<int>(800));
+        // Initialize sliding window lane lines grids
+        window_height_ = 200;
+        window_width_ = 200;
+        curr_sliding_grid_ = std::vector<std::vector<int>>(window_height_, std::vector<int>(window_width_));
+        prev_sliding_grid_ = std::vector<std::vector<int>>(window_height_, std::vector<int>(window_width_));
+        get_pose(prev_pose_x_, prev_pose_y_);
     }
 
 private:
     void publishGrid()
     {
-        std::string fromFrameRel = target_frame_.c_str();
-        std::string toFrameRel = "base_link";
-        geometry_msgs::msg::TransformStamped t;
-
-        // Look up for the transformation between target_frame and turtle2 frames
-        // and send velocity commands for turtle2 to reach target_frame
-        try {
-          t = tf_buffer_->lookupTransform(
-            toFrameRel, fromFrameRel,
-            tf2::TimePointZero);
-        } catch (const tf2::TransformException & ex) {
-          RCLCPP_INFO(
-            this->get_logger(), "Could not transform %s to %s: %s",
-            toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
-          return;
-        }
-
-        double x_pos = t.transform.translation.x,
-        double y_pos = t.transform.translation.y;
+        int global_pose_x;
+        int global_pose_y;
+        get_pose(global_pose_x, global_pose_y);
 
         // Create a sample occupancy grid (replace this with your logic to generate the grid)
         int width = 10;
@@ -85,13 +69,62 @@ private:
         publisher_->publish(occupancyGridMsg);
         RCLCPP_INFO(get_logger(), "Occupancy grid published");
     }
+    void get_pose(double &pose_x, double &pose_y) {
+        std::string fromFrameRel = target_frame_.c_str();
+        std::string toFrameRel = "base_link";
+        geometry_msgs::msg::TransformStamped t;
 
+        // Look up for the transformation between target_frame and turtle2 frames
+        // and send velocity commands for turtle2 to reach target_frame
+        try {
+          t = tf_buffer_->lookupTransform(
+            toFrameRel, fromFrameRel,
+            tf2::TimePointZero);
+        } catch (const tf2::TransformException & ex) {
+          RCLCPP_INFO(
+            this->get_logger(), "Could not transform %s to %s: %s",
+            toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
+          return;
+        }
+
+        pose_x = t.transform.translation.x;
+        pose_y = t.transform.translation.y;
+        
+        return;
+    }
     void cv_grid_callback(const nav_msgs::OccupancyGridPtr &occ_grid) 
     {
+        curr_sliding_grid_ = std::vector<std::vector<int>>(window_height_, std::vector<int>(window_width_));        
+        int resolution = occ_grid->info.resolution;
+
+        int curr_pose_x, curr_pose_y;
+        get_pose(curr_pose_x, curr_pose_y);
+
+        int trans_cols = (curr_pose_x - prev_pose_x_) / resolution;
+        int trans_rows = (curr_pose_y - prev_pose_y_) / resolution;
+        for (int i = 0; i < window_width_; i++) {
+            for (int j = 0; j < window_height_; j++) {
+                int transformed_i = i + trans_rows;
+                int transformed_j = j + trans_cols;
+                if (transformed_i >= 0 && transformed_j >= 0 && transformed_i < window_height_ 
+                        && transformed_j < window_width_) {
+                    curr_sliding_grid_[transformed_i][transformed_j] = prev_sliding_grid_[i][j];
+                }
+            }
+        }
+
+        int cv_height = 50;
+        int cv_width = 100;
+        int top_left_i = window_height_/2 - cv_height_;
+        int top_left_j = window_width_/2 - cv_width_/2;
+        
+
+
+
+
         int global_origin_x = occ_grid->info.origin.position.x;
         int global_origin_y = occ_grid->info.origin.position.y;
 
-        int resolution = occ_grid->info.resolution;
 
         int width = occ_grid->info.width;
         int height = occ_grid->info.height;
@@ -113,9 +146,13 @@ private:
     std::unique_ptr<tf2_ros::Buffer> tf_buffer_;
     std::string target_frame_;
     nav_msgs::OccupancyGrid::Ptr local_grid_;
-    nstd::vector<std::vector<int>> static_grid_;
-    int origin_x_;
-    int origin_y_;
+    std::vector<std::vector<int>> curr_sliding_grid_;
+    std::vector<std::vector<int>> prev_sliding_grid_;
+    int window_height_;
+    int window_width_;
+    double prev_pose_x_;
+    double prev_pose_y_;
+
 };
 
 int main(int argc, char *argv[])
