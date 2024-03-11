c9e8922c39f46bd362f6d6e468b656add3915e53
Ryan4253 <ryan.liao0305@gmail.com>
2024-03-11 05:00:53 +0000

    Refactor gps coordinate calculation

--- gps_service_files/src/GPSCoordinate.cpp
+++ gps_service_files/src/GPSCoordinate.cpp
@@ -1,17 +1,13 @@
 #include "../include/GPSCoordinate.hpp"
 
 long double GPSCoordinate::distanceBetween(const GPSCoordinate& current, const GPSCoordinate& target){
-    const long double latNew = target.getLatitude() * RADIANS_PER_DEGREE;
-    const long double latOld = current.getLatitude() * RADIANS_PER_DEGREE;
-    const long double latDiff = (target.getLatitude() - current.getLatitude()) * RADIANS_PER_DEGREE;
-    const long double lngDiff = (target.getLongitude() - current.getLongitude()) * RADIANS_PER_DEGREE;
+    const long double dLatitude = (target.getLatitude() - current.getLatitude()) * RADIANS_PER_DEGREE;
+    const long double dLongitude = (target.getLongitude() - current.getLongitude()) * RADIANS_PER_DEGREE;
 
-    const double long a = sin(latDiff / 2) * sin(latDiff / 2) + cos(latNew) * cos(latOld) * sin(lngDiff / 2) * sin(lngDiff / 2);
-    const double long c = 2 * atan2(sqrt(a), sqrt(1 - a));
+    const long double a = pow(sin(dLatitude / 2), 2) + pow(sin(dLongitude / 2), 2) * cos(current.getLatitude()) * cos(target.getLatitude());
+    const long double c = 2 * atan2(sqrt(a), sqrt(1 - a));
 
-    const double long distance = TERRESTRIAL_RADIUS_METER * c;
-
-    return distance;
+    return TERRESTRIAL_RADIUS_METER * c;
 }
 
 std::istream& operator>>(std::istream& is, GPSCoordinate& gpsCoordinate){
