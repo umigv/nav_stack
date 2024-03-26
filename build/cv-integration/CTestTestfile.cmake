54cc7f4d40d067ff252c2b0adbc2105f47c26da8
Ben <brossano@umich.edu>
2024-03-26 19:54:57 -0400

    add global pllanner and remove unnecessary folders

--- build/cv-integration/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/umarv/ws/src/nav_stack
-# Build directory: /home/umarv/ws/src/nav_stack/build/cv-integration
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ global_planner/sam_bot_description/launch/planner.launch.py
@@ -0,0 +1,38 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+import os
+
+def generate_launch_description():
+    # Constants
+    planner_directory = get_package_share_directory("sam_bot_description")
+    params_file = os.path.join(planner_directory, 'config', 'custom_fn_params', 'nav2_params.yaml')
+    nav2_directory = get_package_share_directory("nav2_bringup")
+    rviz_config = os.path.join(nav2_directory, 'rviz', 'nav2_default_view.rviz')
+    
+    # RViz node
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config]
+    )
+
+    # Nav2 Node
+    bringup_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(nav2_directory, 'launch', 'bringup_launch.py')),
+        launch_arguments={
+            'slam': 'True',
+            'use_sim_time': 'True',
+            'params_file': params_file,
+            'map': "",
+        }.items(),
+    )
+
+    return LaunchDescription([
+        rviz_node,
+        bringup_cmd,
+    ])
