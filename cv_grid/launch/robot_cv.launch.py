7d3eb5590ebca62f5edd70e8cfb41fe5b84c6a32
Maaz <mdhuss413@gmail.com>
2024-06-01 02:07:40 -0400

    We're so back

---
+++ cv_grid/launch/robot_cv.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterFile
+
+def generate_launch_description():
+    param_file_path = 'src/nav_stack/cv_grid/config/cv_grid.yaml'
+
+    return LaunchDescription([
+        Node(
+            package='drivable_area',
+            executable='drivable_area_test',
+            name='drivable_area',
+            output='screen',
+            parameters=[
+                {'use_sim_time' : True}
+            ],
+            remappings=[
+                ('/occupancy_grid', "/cv_view")
+            ]
+        )
+    ])
