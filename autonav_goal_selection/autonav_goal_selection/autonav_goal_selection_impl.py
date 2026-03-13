2603641ebe675cd56b2021f47a8679d59fec32f6
Ryan Liao <ryan.liao0305@gmail.com>
2026-03-13 14:39:02 -0400

    preliminary heuristic

--- autonav_goal_selection/autonav_goal_selection/autonav_goal_selection_impl.py
+++ autonav_goal_selection/autonav_goal_selection/autonav_goal_selection_impl.py
@@ -27,8 +27,26 @@ def select_goal(
         if not grid.state(point).is_drivable:
             return math.inf
 
-        # This is a bad heuristic but it works fine for open fields
-        return point.distance(waypoint)
+        # this is rotated to the local
+        delta = robot_pose.world_to_local(point)
+
+        # add the zone-based
+        x_weight = 0.0
+        if delta.x < params.behind_robot_penalty_distance_m:
+            x_weight += (params.behind_robot_penalty_distance_m - delta.x) * params.behind_robot_linear_factor
+        y_weight = params.lateral_quadratic_factor * (delta.y**2)
+
+        # because we aren't doing this as a full A*, I don't think we need the prior distance as a factor--it should already be accounted for through zone weight.
+        # the last term here checks if the point is within 1m of the waypoint, and if so, lowers the priority accordingly.
+        return (
+            x_weight
+            + y_weight
+            + grid.state(point).value
+            # this is the waypoint priority hole (within 1m)
+            - (params.waypoint_proximity_weight * (params.waypoint_proximity_radius_m >= point.distance(waypoint)))
+            # waypoint direction priority--adds one lightly weighted term of waypoint distance. just enough to bias the closer side, not enough to even mess with the quadratic.
+            + params.waypoint_dist_weight * point.distance(waypoint)
+        )
 
     best_point = min(grid.in_bound_points(Point2d), key=heuristic)
     return best_point if heuristic(best_point) < math.inf else None
