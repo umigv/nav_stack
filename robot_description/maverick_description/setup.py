c80f3ac8421b242e307f0770c9f6a7138084e2b3
Ryan Liao <ryan.liao0305@gmail.com>
2026-04-14 03:02:11 -0400

    Add maverick_description with urdf

---
+++ robot_description/maverick_description/setup.py
@@ -0,0 +1,27 @@
+from glob import glob
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+package_name = "maverick_description"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (str(Path("share") / package_name / "urdf"), glob("urdf/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Ryan Liao",
+    maintainer_email="ryanliao@umich.edu",
+    description="URDF description for Maverick",
+    license="Apache-2.0",
+    extras_require={},
+    entry_points={
+        "console_scripts": [],
+    },
+)
